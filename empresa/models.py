fcbb7285a994bcdae99d0755abbe22c71ee5e5ab
kawan Nascimento <capar.2022113tads0383@aluno.ifpi.edu.br>
2025-08-14 17:03:09 -0300

    ajeitando os models

--- empresa/models.py
+++ empresa/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from endereco.models import Endereco
+from django.core.validators import MinLengthValidator, RegexValidator
 
 class Empresa(models.Model):
     id = models.AutoField(primary_key=True)
     vNome_fantasia = models.CharField(max_length=80)
     vNome_razao_social = models.CharField(max_length=80)
-    vCnpj = models.CharField(max_length=14, unique=True)
+    vCnpj = models.CharField(
+    max_length=14,
+    
+    unique=True,
+    validators=[
+        MinLengthValidator(14, message="O CNPJ deve ter 14 dígitos"),
+    ]
+)
     vEmail = models.CharField(max_length=40)
     vObservacao = models.CharField(max_length=200, blank=True, null=True)
     iCod_endereco = models.OneToOneField(
